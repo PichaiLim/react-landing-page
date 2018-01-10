40e0dcbcba74ac99d2ffb2f4463c02c9834c08e9
Pichai Limpanitivat <Pichai.Limpanitivat@Gmail.com>
2018-01-10 20:29:15 +0700

    create tempate and using props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/jquery/dist/jquery.min.js";
+
+import NavbarTop from "./NavbarTop";
+import Jumbotron from "./Jumbotron";
+import Card from "./Card";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        {/* TODO props change name */}
+        <NavbarTop titleName="PichaiLim" />
+        <Jumbotron/>
+        <Card/>
       </div>
     );
   }
